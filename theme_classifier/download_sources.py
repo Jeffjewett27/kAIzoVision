19261c08e5817bb3c80ff96ece8deac0ed927238
Jeff <jeffjewett27@gmail.com>
2021-04-21 14:23:47 -0400

    use youtube-dl python interface

--- theme_classifier/download_sources.py
+++ theme_classifier/download_sources.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
 from pathlib import Path
-
+import youtube_dl
 #ranges = pd.read_csv(os.path.join(currDir,"video_data","rangeTable.csv"))
 #videoTable = pd.read_csv(os.path.join(currDir,"video_data","videoTable.csv"))
 
 #videos = videoTable['Id'].unique()
+
+
 
 def download_video(id):
     currDir = Path(__file__).parent
@@ -14,9 +16,15 @@ def download_video(id):
         os.makedirs(sourceDir)
     if (os.path.exists(os.path.join(sourceDir,id + ".mp4"))):
         return
-    cmd = f'youtube-dl.exe -o "{sourceDir}/%(id)s.mp4" -f mp4 https://www.youtube.com/watch?v=' + id
-    print("Executing: " + cmd)
-    os.system(cmd)
+    ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': f'{sourceDir}/%(id)s.mp4'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading: " + id)
+        ydl.download(['https://www.youtube.com/watch?v=' + id])
+    #cmd = f'youtube-dl.exe -o "{sourceDir}/%(id)s.mp4" -f mp4 https://www.youtube.com/watch?v=' + id
+    #os.system(cmd)
 
 def delete_video(id):
     currDir = Path(__file__).parent
